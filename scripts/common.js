a055eb6842651c4995b6669d4fedb45d469d6466
kenchen20050821 <kenchen20050821@hotmail.com>
2012-02-02 22:31:23 +0800

    by siemon

---
+++ scripts/common.js
@@ -0,0 +1,259 @@
+﻿var jsmenu = new Array();
+var ctrlobjclassName;
+jsmenu['active'] = new Array();
+jsmenu['timer'] = new Array();
+jsmenu['iframe'] = new Array();
+
+
+var JSMENU = [];
+JSMENU['active'] = [];
+JSMENU['timer'] = [];
+JSMENU['drag'] = [];
+JSMENU['layer'] = 0;
+JSMENU['zIndex'] = {'win':200,'menu':300,'prompt':400,'dialog':500};
+JSMENU['float'] = '';
+function isUndefined(variable) {
+	return typeof variable == 'undefined' ? true : false;
+}
+function showMenu(v) {
+	var ctrlid = isUndefined(v['ctrlid']) ? v : v['ctrlid'];
+	var showid = isUndefined(v['showid']) ? ctrlid : v['showid'];
+	var menuid = isUndefined(v['menuid']) ? showid + '_menu' : v['menuid'];
+	var ctrlObj = $(ctrlid);
+	var menuObj = $(menuid);
+	if(!menuObj) return;
+	var mtype = isUndefined(v['mtype']) ? 'menu' : v['mtype'];
+	var evt = isUndefined(v['evt']) ? 'mouseover' : v['evt'];
+	var pos = isUndefined(v['pos']) ? '43' : v['pos'];
+	var layer = isUndefined(v['layer']) ? 1 : v['layer'];
+	var duration = isUndefined(v['duration']) ? 2 : v['duration'];
+	var timeout = isUndefined(v['timeout']) ? 250 : v['timeout'];
+	var maxh = isUndefined(v['maxh']) ? 600 : v['maxh'];
+	var cache = isUndefined(v['cache']) ? 1 : v['cache'];
+	var drag = isUndefined(v['drag']) ? '' : v['drag'];
+	var dragobj = drag && $(drag) ? $(drag) : menuObj;
+	var fade = isUndefined(v['fade']) ? 0 : v['fade'];
+	var cover = isUndefined(v['cover']) ? 0 : v['cover'];
+	var zindex = isUndefined(v['zindex']) ? JSMENU['zIndex']['menu'] : v['zindex'];
+	zindex = cover ? zindex + 200 : zindex;
+	if(typeof JSMENU['active'][layer] == 'undefined') {
+		JSMENU['active'][layer] = [];
+	}
+
+	if(evt == 'click' && in_array(menuid, JSMENU['active'][layer]) && mtype != 'win') {
+		hideMenu(menuid, mtype);
+		return;
+	}
+	if(mtype == 'menu') {
+		hideMenu(layer, mtype);
+	}
+
+	if(ctrlObj) {
+		//note 初始化ctrlObj
+		if(!ctrlObj.initialized) {
+			ctrlObj.initialized = true;
+			ctrlObj.unselectable = true;
+
+			ctrlObj.outfunc = typeof ctrlObj.onmouseout == 'function' ? ctrlObj.onmouseout : null;
+			ctrlObj.onmouseout = function() {
+				if(this.outfunc) this.outfunc();
+				if(duration < 3 && !JSMENU['timer'][menuid]) JSMENU['timer'][menuid] = setTimeout('hideMenu(\'' + menuid + '\', \'' + mtype + '\')', timeout);
+			};
+
+			ctrlObj.overfunc = typeof ctrlObj.onmouseover == 'function' ? ctrlObj.onmouseover : null;
+			ctrlObj.onmouseover = function(e) {
+				doane(e);
+				if(this.overfunc) this.overfunc();
+				if(evt == 'click') {
+					clearTimeout(JSMENU['timer'][menuid]);
+					JSMENU['timer'][menuid] = null;
+				} else {
+					for(var i in JSMENU['timer']) {
+						if(JSMENU['timer'][i]) {
+							clearTimeout(JSMENU['timer'][i]);
+							JSMENU['timer'][i] = null;
+						}
+					}
+				}
+			};
+		}
+	}
+
+	var dragMenu = function(menuObj, e, op) {
+		e = e ? e : window.event;
+		if(op == 1) {
+			if(in_array(BROWSER.ie ? e.srcElement.tagName : e.target.tagName, ['TEXTAREA', 'INPUT', 'BUTTON', 'SELECT'])) {
+				return;
+			}
+			JSMENU['drag'] = [e.clientX, e.clientY];
+			JSMENU['drag'][2] = parseInt(menuObj.style.left);
+			JSMENU['drag'][3] = parseInt(menuObj.style.top);
+			document.onmousemove = function(e) {try{dragMenu(menuObj, e, 2);}catch(err){}};
+			document.onmouseup = function(e) {try{dragMenu(menuObj, e, 3);}catch(err){}};
+			doane(e);
+		}else if(op == 2 && JSMENU['drag'][0]) {
+			var menudragnow = [e.clientX, e.clientY];
+			menuObj.style.left = (JSMENU['drag'][2] + menudragnow[0] - JSMENU['drag'][0]) + 'px';
+			menuObj.style.top = (JSMENU['drag'][3] + menudragnow[1] - JSMENU['drag'][1]) + 'px';
+			doane(e);
+		}else if(op == 3) {
+			JSMENU['drag'] = [];
+			document.onmousemove = null;
+			document.onmouseup = null;
+		}
+	};
+
+	//note 初始化menuObj
+	if(!menuObj.initialized) {
+		menuObj.initialized = true;
+		menuObj.ctrlkey = ctrlid;
+		menuObj.mtype = mtype;
+		menuObj.layer = layer;
+		menuObj.cover = cover;
+		if(ctrlObj && ctrlObj.getAttribute('fwin')) {menuObj.scrolly = true;}
+		menuObj.style.position = 'absolute';
+		menuObj.style.zIndex = zindex + layer;
+		menuObj.onclick = function(e) {
+			if(!e || BROWSER.ie) {
+				window.event.cancelBubble = true;
+				return window.event;
+			} else {
+				e.stopPropagation();
+				return e;
+			}
+		};
+		if(duration < 3) {
+			if(duration > 1) {
+				menuObj.onmouseover = function() {
+					clearTimeout(JSMENU['timer'][menuid]);
+					JSMENU['timer'][menuid] = null;
+				};
+			}
+			if(duration != 1) {
+				menuObj.onmouseout = function() {
+					JSMENU['timer'][menuid] = setTimeout('hideMenu(\'' + menuid + '\', \'' + mtype + '\')', timeout);
+				};
+			}
+		}
+		if(cover) {
+			var coverObj = document.createElement('div');
+			coverObj.id = menuid + '_cover';
+			coverObj.style.position = 'absolute';
+			coverObj.style.zIndex = menuObj.style.zIndex - 1;
+			coverObj.style.left = coverObj.style.top = '0px';
+			coverObj.style.width = '100%';
+			coverObj.style.height = document.body.offsetHeight + 'px';
+			coverObj.style.backgroundColor = '#000';
+			coverObj.style.filter = 'progid:DXImageTransform.Microsoft.Alpha(opacity=50)';
+			coverObj.style.opacity = 0.5;
+			$('append_parent').appendChild(coverObj);
+			_attachEvent(window, 'load', function () {
+				coverObj.style.height = document.body.offsetHeight + 'px';
+			}, document);
+		}
+	}
+	if(drag) {
+		dragobj.style.cursor = 'move';
+		dragobj.onmousedown = function(event) {try{dragMenu(menuObj, event, 1);}catch(e){}};
+	}
+	menuObj.style.display = '';
+	if(cover) $(menuid + '_cover').style.display = '';
+	if(fade) {
+		var O = 0;
+		var fadeIn = function(O) {
+			if(O == 100) {
+				clearTimeout(fadeInTimer);
+				return;
+			}
+			menuObj.style.filter = 'progid:DXImageTransform.Microsoft.Alpha(opacity=' + O + ')';
+			menuObj.style.opacity = O / 100;
+			O += 10;
+			var fadeInTimer = setTimeout(function () {
+				fadeIn(O);
+			}, 50);
+		};
+		fadeIn(O);
+		menuObj.fade = true;
+	} else {
+		menuObj.fade = false;
+	}
+	if(pos != '*') {
+		setMenuPosition(showid, menuid, pos);
+	}
+	if(maxh && menuObj.scrollHeight > maxh) {
+		menuObj.style.height = maxh + 'px';
+		if(BROWSER.opera) {
+			menuObj.style.overflow = 'auto';
+		} else {
+			menuObj.style.overflowY = 'auto';
+		}
+	}
+
+	if(!duration) {
+		setTimeout('hideMenu(\'' + menuid + '\', \'' + mtype + '\')', timeout);
+	}
+
+	if(!in_array(menuid, JSMENU['active'][layer])) JSMENU['active'][layer].push(menuid);
+	menuObj.cache = cache;
+	if(layer > JSMENU['layer']) {
+		JSMENU['layer'] = layer;
+	}
+}
+
+/**
+* 隐藏菜单
+* attr	菜单属性 空字串:隐藏所有菜单 字串:隐藏指定menuid的菜单 数字:隐藏某一层菜单
+* mtype	菜单类型 参见showMenu函数
+*/
+function hideMenu(attr, mtype) {
+	attr = isUndefined(attr) ? '' : attr;
+	mtype = isUndefined(mtype) ? 'menu' : mtype;
+	if(attr == '') {
+		for(var i = 1; i <= JSMENU['layer']; i++) {
+			hideMenu(i, mtype);
+		}
+		return;
+	} else if(typeof attr == 'number') {
+		for(var j in JSMENU['active'][attr]) {
+			hideMenu(JSMENU['active'][attr][j], mtype);
+		}
+		return;
+	}else if(typeof attr == 'string') {
+		var menuObj = $(attr);
+		if(!menuObj || (mtype && menuObj.mtype != mtype)) return;
+		clearTimeout(JSMENU['timer'][attr]);
+		var hide = function () {
+		    if (menuObj.cache) {
+		        menuObj.style.display = 'none';
+		        if (menuObj.cover) $(attr + '_cover').style.display = 'none';
+		    } else {
+		        menuObj.parentNode.removeChild(menuObj);
+		        if (menuObj.cover) $(attr + '_cover').parentNode.removeChild($(attr + '_cover'));
+		    }
+		    var tmp = [];
+		    for (var k in JSMENU['active'][menuObj.layer]) {
+		        if (attr != JSMENU['active'][menuObj.layer][k]) tmp.push(JSMENU['active'][menuObj.layer][k]);
+		    }
+		    JSMENU['active'][menuObj.layer] = tmp;
+		};
+		if(menuObj.fade) {
+			var O = 100;
+			var fadeOut = function(O) {
+				if(O == 0) {
+					clearTimeout(fadeOutTimer);
+					hide();
+					return;
+				}
+				menuObj.style.filter = 'progid:DXImageTransform.Microsoft.Alpha(opacity=' + O + ')';
+				menuObj.style.opacity = O / 100;
+				O -= 10;
+				var fadeOutTimer = setTimeout(function () {
+					fadeOut(O);
+				}, 50);
+			};
+			fadeOut(O);
+		} else {
+			hide();
+		}
+	}
+}
